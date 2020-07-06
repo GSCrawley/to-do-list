a3e63aa59336682c081c30d249c2d5a4f16a0af3
GSCrawley <gscrawley@gmail.com>
2020-07-06 15:25:46 -0700

    added TodoList component

--- to-do-list/src/index.js
+++ to-do-list/src/index.js
@@ -13,7 +13,7 @@ render(
   </Provider>,
   document.getElementById('root')
 )
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
