31d23df7eb3a0e4bf159a9edb785026cff3a9b49
GSCrawley <gscrawley@gmail.com>
2020-07-15 14:32:11 -0700

    Updates

---
+++ precache-manifest.449b227964f94603c8f253f772065920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d18a6f8bf4ea256dd6a43c3cd34988f",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "25b0e2fe841144acdf68",
+    "url": "/to-do-list/static/js/2.866b75d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do-list/static/js/2.866b75d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117ee25a52f6209bface",
+    "url": "/to-do-list/static/js/main.cba3365d.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
